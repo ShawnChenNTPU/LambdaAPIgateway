66930044cc03dd4c9b54a81b40bde7c9e7050d9a
shawn <73820271+ShawnChenNTPU@users.noreply.github.com>
2022-02-17 21:55:54 +0800

    Update testAPI.py

--- testAPI.py
+++ testAPI.py
@@ -3,7 +3,7 @@ import urllib3
 http = urllib3.PoolManager()
 
 def CTF(ueID):
-    data = json.dumps({'ue_ID':ueID\})
+    data = json.dumps({'ue_ID':ueID})
     URL='https://31ce7rsi31.execute-api.us-east-1.amazonaws.com/s1/registration'
     response = http.request('POST',URL, body=data)
     word='registration ok'
